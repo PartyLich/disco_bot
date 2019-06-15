e4098dc0fa1073eaf7b9a5af97720d1f7d24d755
Arthur E. Jones <PartyLich@gmail.com>
2019-06-15 18:46:03 -0400

    feat: add volume command

--- index.js
+++ index.js
@@ -45,6 +45,10 @@ client.on('message', async (message) => {
       stop(message, serverQueue);
       return;
 
+    case `${prefix}vol`:
+      louder(message, serverQueue);
+      return;
+
     default:
       message.channel.send('You need to enter a valid command!');
   }
@@ -171,3 +175,49 @@ function stop(message, serverQueue) {
   );
 }
 
+function louder(message, serverQueue) {
+  if (!serverQueue) {
+    return message.channel.send(
+        `There is only silence ${message.author.username}.`
+    );
+  }
+
+  const uppers = new Set(['up', 'louder']);
+  const downers = new Set(['down', 'quieter']);
+  const args = message.content.split(' ');
+  const volume = parseInt(args[1]);
+  const volUp = `(volume up)`;
+  const volDown = `(volume down)`;
+
+  if (isNaN(volume)) {
+    if (uppers.has(args[1])) {
+      serverQueue.volume = Math.min(5, serverQueue.volume + 1);
+      serverQueue.connection.dispatcher.setVolumeLogarithmic(
+          serverQueue.volume / 5
+      );
+      return message.channel.send(volUp);
+    } else if (downers.has(args[1])) {
+      serverQueue.volume = Math.max(0, serverQueue.volume - 1);
+      serverQueue.connection.dispatcher.setVolumeLogarithmic(
+          serverQueue.volume / 5
+      );
+      return message.channel.send(volDown);
+    }
+    return message.channel.send(
+        `Sorry ${message.author.username}, ${args[1]} isn't a volume I understand`
+    );
+  }
+
+  if (volume > 5 || volume < 0) {
+    return message.channel.send(
+        `Sorry ${message.author.username}, ${args[1]} must be between 0 and 5 inclusive.`
+    );
+  } else {
+    const response = serverQueue.volume > volume ? volDown : volUp;
+    serverQueue.volume = volume;
+    serverQueue.connection.dispatcher.setVolumeLogarithmic(
+        serverQueue.volume / 5
+    );
+    return message.channel.send(`${response}`);
+  }
+}
