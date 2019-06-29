d7789fe9309b2c362584469c942f5ff45c2ff23b
Arthur E. Jones <PartyLich@gmail.com>
2019-06-29 15:33:58 -0400

    feat: add help command

--- index.js
+++ index.js
@@ -60,6 +60,12 @@ client.on('message', async (message) => {
       cleanMessage(message);
       return;
 
+    case `${prefix}halp`:
+    case `${prefix}help`:
+      help(message);
+      cleanMessage(message);
+      return;
+
     default:
       message.channel.send('You need to enter a valid command!');
   }
@@ -265,5 +271,31 @@ function repeat(message, serverQueue) {
       `Repeating ${song.title} for ${
         message.author.username
       }`
+  );
+}
+
+function help(message) {
+  let commandList = [
+    '```',
+    `${prefix}play [youtube url]: add the specified url to the play queue`,
+    `  e.g. !play`,
+    `${prefix}skip: move to the next song immediately`,
+    `  e.g. !skip`,
+    `${prefix}stop: stop all playback`,
+    `  e.g. !stop`,
+    `${prefix}next: get the next song title in the queue`,
+    `  e.g. !next`,
+    `${prefix}repeat:  play the current song again`,
+    `  e.g.!repeat`,
+    `${prefix}vol [0-5, up, down]: adjust the volume`,
+    `  e.g. !vol 3`,
+    `${prefix}halp: The text you're reading right now! (alias: ${prefix}help)`,
+    '```',
+  ];
+
+  commandList = commandList.reduce((prev, next) => prev + '\n' + next);
+
+  return message.channel.send(
+      `${message.author.username}, I can do the following:\n${commandList}`
   );
 }
