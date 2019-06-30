3220f39037fff71b47dba5ec744fd3a1f29c7e3b
Arthur E. Jones <PartyLich@gmail.com>
2019-06-30 19:17:21 -0400

    refactor: extract play to own module

---
+++ commands/play.js
@@ -0,0 +1,142 @@
+import path from 'path';
+import ytdl from 'ytdl-core';
+import {DIALOG} from '../dialog.json';
+import {randInt} from '../randInt.js';
+
+const name = 'play';
+const description = 'Play the requested song';
+
+export {
+  name,
+  description,
+  execute,
+};
+
+/**
+ * Check if the user is in a voice chat and if the bot has the right permission.
+ *  If not we write an error message and return.
+ * @param  {Message} message     The Discord message we're responding to
+ * @param  {Object} serverQueue the contract for our song queue
+ * @return {Promise}             Promise for the bot's reply message
+ */
+async function execute(message, {serverQueue, args}) {
+  const voiceChannel = message.member.voiceChannel;
+  const queue = args[args.length -1];
+
+  // check if user is in voice channel
+  if (!voiceChannel) {
+    return message.channel.send(
+        'You need to be in a voice channel to play music!'
+    );
+  }
+  // check if the bot has the right permissions
+  const permissions = voiceChannel.permissionsFor(message.client.user);
+  if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
+    return message.channel.send(
+        'I need the permissions to join and speak in your voice channel!'
+    );
+  }
+
+  // use ytdl library to get the song information from the youtube link
+  const songInfo = await ytdl.getInfo(args[0]);
+  console.log(songInfo);
+  // use ffmpeg to mix song and voiceline
+  const songFile = '';
+  // const songFile = await '';
+
+  const song = {
+    title: songInfo.title,
+    url: songInfo.video_url,
+    file: songFile,
+  };
+
+  const dialog = DIALOG.play;
+
+  // check if music is already playing.
+  if (!serverQueue) {
+    // Create the contract for our queue
+    serverQueue = {
+      textChannel: message.channel,
+      voiceChannel: voiceChannel,
+      connection: null, // {VoiceConnection}
+      songs: [],
+      volume: 5,
+      playing: true,
+    };
+    // Set the queue using our contract
+    queue.set(message.guild.id, serverQueue);
+    // add the song to list
+    serverQueue.songs.push(song);
+
+    try {
+      // join the voicechat and save our connection
+      const connection = await voiceChannel.join();
+      console.log(`Connected to ${voiceChannel.name}!`);
+      serverQueue.connection = connection;
+
+      // Start a song
+      play(message, {serverQueue, args: [serverQueue.songs[0], queue]});
+
+      return message.channel.send(
+          `${dialog[randInt(dialog.length - 1)]}` +
+          `\n${song.title} has been added to the queue!`
+      );
+    } catch (err) {
+      // Print error message if the bot fails to join the voicechat
+      console.error(err);
+      queue.delete(message.guild.id);
+      return message.channel.send(err);
+    }
+  } else {
+    // Add the song to our existing serverQueue and send a success message.
+    serverQueue.songs.push(song);
+    console.log(serverQueue.songs);
+
+    return message.channel.send(
+        `${dialog[randInt(dialog.length - 1)]}` +
+        `\n${song.title} has been added to the queue!`
+    );
+  }
+}
+
+/**
+ * Start playback of a song
+ * @param  {Message} message     The Discord message we're responding to
+ * @param  {Object} serverQueue the contract for our song queue
+ * @param  {Array} args  array of other arguments
+ * @return {Promise}             Promise for the bot's reply message
+ */
+async function play(message, {serverQueue, args: [song, ...args]} = {}) {
+  // if the song is empty, leave the voice channel and delete the queue.
+  const queue = args[args.length - 1];
+  if (!song) {
+    serverQueue.voiceChannel.leave();
+    queue.delete(message.guild.id);
+    return false;
+  }
+
+  // start playback using the playStream() function and the URL of our song.
+  const opts = {filter: 'audioonly'};
+  const streamOptions = {seek: 0, volume: 1};
+  const songStream = ytdl(song.url, opts);
+  let dispatcher = serverQueue.connection
+      .playFile(path.resolve('./lucio/Lúcio - Major Buzzkill.m4a'))
+      .on('end', () => {
+        dispatcher = serverQueue.connection
+            .playStream(songStream, streamOptions)
+            .on('end', () => {
+              console.log(`Playback ended: ${song.title}`);
+              // play the next song and remove completed song from queue
+              serverQueue.songs.shift();
+              play(message, {serverQueue, args: [serverQueue.songs[0], queue]});
+            })
+            .on('error', (error) => {
+              console.error(error);
+            });
+      })
+      .on('error', (error) => {
+        console.error(error);
+      });
+
+  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+}
--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 import fs from 'fs';
 import Discord from 'discord.js';
-import ytdl from 'ytdl-core';
 import {prefix, token} from './config.json';
-import {DIALOG} from './dialog.json';
-import {randInt} from './randInt.js';
 import {cleanMessage} from './cleanMessage.js';
 
 // create client
@@ -33,10 +30,10 @@ client.once('ready', () => {
   console.log('ready');
 });
 client.once('Reconnecting', () => {
-  console.log('reconnecting');
+  console.log('reconnecting...');
 });
 client.once('disconnect', () => {
-  console.log('disconnecting');
+  console.log('disconnecting...');
 });
 
 // message event listener
@@ -54,6 +51,7 @@ client.on('message', async (message) => {
 
   if (client.commands.has(command)) {
     try {
+      args.push(queue);
       client.commands.get(command).execute(message, {serverQueue, args});
       cleanMessage(message);
     } catch (e) {
@@ -61,119 +59,4 @@ client.on('message', async (message) => {
       message.reply('There was an error executing that command');
     }
   }
-
-  switch (command) {
-    case `play`:
-      execute(message, {serverQueue, args});
-      cleanMessage(message);
-      return;
-
-    default:
-  }
 });
-
-/**
- * Check if the user is in a voice chat and if the bot has the right permission.
- *  If not we write an error message and return.
- * @param  {Message} message     The Discord message we're responding to
- * @param  {Object} serverQueue the contract for our song queue
- * @return {Promise}             Promise for the bot's reply message
- */
-async function execute(message, {serverQueue, args}) {
-  const voiceChannel = message.member.voiceChannel;
-
-  // check if user is in voice channel
-  if (!voiceChannel) {
-    return message.channel.send(
-        'You need to be in a voice channel to play music!'
-    );
-  }
-  // check if the bot has the right permissions
-  const permissions = voiceChannel.permissionsFor(message.client.user);
-  if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
-    return message.channel.send(
-        'I need the permissions to join and speak in your voice channel!'
-    );
-  }
-
-  // use ytdl library to get the song information from the youtube link
-  const songInfo = await ytdl.getInfo(args[0]);
-  const song = {
-    title: songInfo.title,
-    url: songInfo.video_url,
-  };
-
-  const dialog = DIALOG.play;
-
-  // check if music is already playing.
-  if (!serverQueue) {
-    // Create the contract for our queue
-    const queueContract = {
-      textChannel: message.channel,
-      voiceChannel: voiceChannel,
-      connection: null, // {VoiceConnection}
-      songs: [],
-      volume: 5,
-      playing: true,
-    };
-    // Set the queue using our contract
-    queue.set(message.guild.id, queueContract);
-    // add the song to list
-    queueContract.songs.push(song);
-
-    try {
-      // join the voicechat and save our connection
-      const connection = await voiceChannel.join();
-      console.log(`Connected to ${voiceChannel.name}!`);
-      queueContract.connection = connection;
-      // Start a song
-      play(message.guild, queueContract.songs[0]);
-
-      message.channel.send(`${song.title} has been added to the queue!`);
-      return message.channel.send(`${dialog[randInt(dialog.length - 1)]}`);
-    } catch (err) {
-      // Print error message if the bot fails to join the voicechat
-      console.error(err);
-      queue.delete(message.guild.id);
-      return message.channel.send(err);
-    }
-  } else {
-    // Add the song to our existing serverQueue and send a success message.
-    serverQueue.songs.push(song);
-    console.log(serverQueue.songs);
-
-    message.channel.send(`${song.title} has been added to the queue!`);
-    return message.channel.send(`${dialog[randInt(dialog.length - 1)]}`);
-  }
-}
-
-/**
- * Start playback of a song
- * @param  {!guild} guild  The server to play in
- * @param  {!Object} song The song obj to be played
- */
-function play(guild, song) {
-  const serverQueue = queue.get(guild.id);
-  // if the song is empty, leave the voice channel and delete the queue.
-  if (!song) {
-    serverQueue.voiceChannel.leave();
-    queue.delete(guild.id);
-    return;
-  }
-  // start playback using the playStream() function and the URL of our song.
-  const opts = {filter: 'audioonly'};
-  const streamOptions = {seek: 0, volume: 1};
-  const songStream = ytdl(song.url, opts);
-  const dispatcher = serverQueue.connection
-      .playStream(songStream, streamOptions)
-      .on('end', () => {
-        console.log(`Playback ended: ${song.title}`);
-        // play the next song and remove completed song from queue
-        play(guild, serverQueue.songs.shift());
-      })
-      .on('error', (error) => {
-        console.error(error);
-      });
-
-  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-}
