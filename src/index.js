d4185b1f32069ac4bca40bb4bd7aa5ab4f294945
Arthur E. Jones <PartyLich@gmail.com>
2019-07-06 22:11:39 -0400

    fix: fix command file loading
    Directory structure updates require changes here.

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ client.login(token);
 // import client commands
 client.commands = new Discord.Collection();
 const commandFiles = fs
-    .readdirSync('./commands')
+    .readdirSync('./src/commands')
     .filter((file) => file.endsWith('.js'));
 
 for (const file of commandFiles) {
