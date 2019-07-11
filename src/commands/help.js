835f0bd8cc5ddd5739fc3206b823878ced836139
Arthur E. Jones <PartyLich@gmail.com>
2019-07-10 20:26:20 -0400

    feat: generate help dynamically
    Generate help command dynamically based on the content of other loaded
    commands. Eases future update burdens.
    
    close #9

--- src/commands/help.js
+++ src/commands/help.js
@@ -17,25 +17,21 @@ export {
  * @return {Promise}             Promise for the bot's reply message
  */
 function execute(message) {
+  const {commands} = message.client;
   const user = message.member.nickname;
-  const commandList = [
-    `${user}, I can do the following:`,
-    '```',
-    `${prefix}play [youtube url]: add the specified url to the play queue`,
-    `  e.g. !play`,
-    `${prefix}skip: move to the next song immediately`,
-    `  e.g. !skip`,
-    `${prefix}stop: stop all playback`,
-    `  e.g. !stop`,
-    `${prefix}next: get the next song title in the queue`,
-    `  e.g. !next`,
-    `${prefix}repeat:  play the current song again`,
-    `  e.g.!repeat`,
-    `${prefix}vol [0-5, up, down]: adjust the volume`,
-    `  e.g. !vol 3`,
-    `${prefix}halp: The text you're reading right now! (alias: ${prefix}help)`,
-    '```',
-  ];
+  const commandList = [];
+
+  // build output string
+  commandList.push(`**${user}**, I can do the following:`);
+  commandList.push('```bash');
+
+  for (const [, command] of commands) {
+    commandList.push(`${prefix + command.name} ${command.description}`);
+    if (command.alias) commandList.push(`  Aliases: ${command.alias}`);
+    if (command.usage) commandList.push(`  Usage: ${prefix + command.usage}`);
+  }
+
+  commandList.push('```');
 
   return message.channel.send(commandList);
 }
