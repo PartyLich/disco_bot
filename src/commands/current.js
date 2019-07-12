30dcd352568508545af023d6610c6d59dc7d3592
Arthur E. Jones <PartyLich@gmail.com>
2019-07-12 18:35:22 -0400

    docs: update documentation/comments

--- src/commands/current.js
+++ src/commands/current.js
@@ -16,6 +16,12 @@ export {
   execute,
 };
 
+/**
+ * Display currently playing song info
+ * @param  {Message}   message     The Discord message we're responding to
+ * @param  {Object}   serverQueue the contract for our song queue
+ * @return {Promise}             Promise for the bot's reply message
+ */
 function execute(message, {serverQueue: {songs = []}, args} = {}) {
   if (songs.length < 1) {
     return message.channel.send(`There's nothing in the queue.`);
