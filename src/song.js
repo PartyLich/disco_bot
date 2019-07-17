8ebe7ff66130504852348eb1ec9714ffc052ec08
Arthur E. Jones <PartyLich@gmail.com>
2019-07-17 14:45:25 -0400

    refactor: extract Song obj creation to own class
    Support reusability and other nice principles.

---
+++ src/song.js
@@ -0,0 +1,16 @@
+export default function Song(songInfo, requestor) {
+  const songFile = '';
+  const lenSeconds = ('00' + (songInfo.length_seconds % 60)).slice(-2);
+  const lenMinutes = ('00' + Math.floor(songInfo.length_seconds / 60)).slice(
+      -2
+  );
+  const lenString = `${lenMinutes}:${lenSeconds}`;
+
+  return {
+    title: songInfo.title,
+    url: songInfo.video_url,
+    file: songFile,
+    length: lenString,
+    requestor,
+  };
+}
