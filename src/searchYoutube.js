d93a37e00eadc77c078843bd143804978ab1f9d1
Arthur E. Jones <PartyLich@gmail.com>
2019-07-16 20:57:27 -0400

    feat: add youtube API search function

--- src/searchYoutube.js
+++ src/searchYoutube.js
@@ -1,5 +1,22 @@
 import https from 'https';
 
+async function searchYoutube(query, topic, apiKey, maxResults) {
+  const baseUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet';
+  const type = '&type=video';
+  const _maxResults = `&maxResults=${maxResults}`;
+  const sortOrder = '&order=relevance';
+  const _query = `&q=${encodeURIComponent(query)}`;
+  const _topic = `&topicId=${encodeURIComponent(topic)}`;
+  const _apiKey = `&key=${apiKey}`;
+  const request = `${baseUrl}${_maxResults}${sortOrder}${_query}${_topic}${_apiKey}${type}`;
+
+  try {
+    const response = await fetch(request);
+    return JSON.parse(response);
+  } catch (err) {
+    console.error(err.message);
+  }
+}
 
 /**
  * node.js https.get with a promise wrapper
