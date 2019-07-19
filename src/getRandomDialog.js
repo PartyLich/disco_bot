19b44154615d0996fb5a7083326717cbfb7bb8ff
Arthur E. Jones <PartyLich@gmail.com>
2019-07-19 19:54:10 -0400

    docs: update documentation/comments

--- src/getRandomDialog.js
+++ src/getRandomDialog.js
@@ -2,6 +2,11 @@ import {randInt} from './randInt';
 
 export default getRandomDialog;
 
+/**
+ * Return randomly selected string from dialog array
+ * @param  {Array} dialog dialog options
+ * @return {string}        randomly selected dialog
+ */
 function getRandomDialog(dialog) {
   return `${dialog[randInt(dialog.length - 1)]}`;
 }
